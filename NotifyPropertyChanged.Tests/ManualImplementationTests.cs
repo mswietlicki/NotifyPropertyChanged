4f9c09877c6796654b7e778be99e67fa40ec4cef
Mateusz Świetlicki <mateusz@swietlicki.net>
2014-06-21 00:36:50 +0200

    Init

---
+++ NotifyPropertyChanged.Tests/ManualImplementationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotifyPropertyChanged.Manual;
+using Xunit;
+
+namespace NotifyPropertyChanged.Tests
+{
+    public class ManualImplementationTests
+    {
+        [Fact]
+        public void FirstNamePropertyChanged()
+        {
+            var firstNameChanged = false;
+            var person = new Person();
+            person.PropertyChanged += (sender, args) => { if (args.PropertyName == "FirstName") firstNameChanged = true; };
+
+            person.FirstName = "Mateusz";
+
+            Assert.True(firstNameChanged);
+        }
+
+        [Fact]
+        public void FullNamePropertyChangedOnFirstNameChange()
+        {
+            var fullNameChanged = false;
+            var person = new Person();
+            person.PropertyChanged += (sender, args) => { if (args.PropertyName == "FullName") fullNameChanged = true; };
+
+            person.FirstName = "Mateusz";
+
+            Assert.True(fullNameChanged);
+        }
+
+    }
+}
