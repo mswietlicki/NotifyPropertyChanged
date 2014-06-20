bf1cf48d78e95fd4519cfb8a8311727e2712104a
Mateusz Świetlicki <mateusz@swietlicki.net>
2014-06-21 01:32:59 +0200

    Added PropertyChanged.Fody

---
+++ NotifyPropertyChanged/1.Manual/Person.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NotifyPropertyChanged.Manual
+{
+    public class Person: INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged("FullName");
+            }
+        }
+
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ NotifyPropertyChanged/Fody/Person.cs
@@ -0,0 +1,17 @@
+﻿using PropertyChanged;
+
+namespace NotifyPropertyChanged.Fody
+{
+    [ImplementPropertyChanged]
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+    }
+}
